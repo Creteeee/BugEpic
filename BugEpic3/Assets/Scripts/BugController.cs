d7565650e64904167986de5d3cbd3585284fab8e
Crete <140468648+Creteeee@users.noreply.github.com>
2025-10-16 22:17:42 +0800

    过场虫子走+荷马说话

--- BugEpic3/Assets/Scripts/BugController.cs
+++ BugEpic3/Assets/Scripts/BugController.cs
@@ -7,28 +7,31 @@ using UnityEngine;
 
 public class BugController : Singleton<BugController>
 {
+    [Header("虫子状态")]
     public BugStates bugStates = BugStates.Move;
     public GameObject bugSprite;
+    [Header("路径")]
+    public GameManager.UIType uiType = GameManager.UIType.NonDialogue;
     public Material footPrintMat;
     public GameObject wayPointsGroup;
     public Transform[] waypoints;
-    public float duration = 3f;
+    public float Speed = 3f;
     private Tween pathTween;
     private Vector3[] path;
-    public Collider2D cd;
-    
+ 
    
     void Start()
     {
         //查找wayPointsGroup
         //返回wayPointsFroup下的所有子物体
+        InitializeWayPointsGroup();
         waypoints = wayPointsGroup.GetComponentsInChildren<Transform>()
             .Skip(1)
             .ToArray();
-        cd = this.transform.GetComponent<Collider2D>();
+       
     }
 
-    // Update is called once per frame
+ 
     void Update()
     {
         if (bugStates == BugStates.Move)
@@ -37,10 +40,18 @@ public class BugController : Singleton<BugController>
         }
         
     }
-    
+    /// <summary>
+    /// 虫子是活动的还是静止的
+    /// </summary>
     public enum BugStates
     {
         Move,Still,Talk
+    }
+
+
+    public void ChangeState(BugStates state)
+    {
+        bugStates = state;
     }
 
     void BugMove()
@@ -49,18 +60,33 @@ public class BugController : Singleton<BugController>
         for (int i = 0; i < waypoints.Length; i++)
             path[i] = waypoints[i].position;
 
-        // 启动 DOTween 路径动画
-        pathTween = transform.DOPath(path, duration, PathType.CatmullRom, PathMode.Ignore)
-            .SetEase(Ease.Linear)
-            .OnUpdate(UpdateOrientation)
-            .OnComplete(SHowPV);
+        switch (uiType)
+        {
+            case GameManager.UIType.NonDialogue:
+                GameManager.Instance.playerState = GameManager.PlayerState.Active;
+                pathTween = transform.DOPath(path, (1/Speed) * waypoints.Length, PathType.CatmullRom, PathMode.Ignore)
+                    .SetEase(Ease.Linear)
+                    .OnUpdate(UpdateOrientation)
+                    .OnComplete(SlideUI);
+                break;
+            case GameManager.UIType.Dialogue:
+                GameManager.Instance.playerState = GameManager.PlayerState.Dialogue;
+                
+                
+                break;
+            case GameManager.UIType.Beginning:
+                GameManager.Instance.playerState = GameManager.PlayerState.Active;
+                pathTween = transform.DOPath(path, (1/Speed) * waypoints.Length, PathType.CatmullRom, PathMode.Ignore)
+                    .SetEase(Ease.Linear)
+                    .OnUpdate(UpdateOrientation)
+                    .OnComplete(SHowPV);
+                break;
+                
+        }
+
         bugStates = BugStates.Still;
     }
-
-    void ChangeState(BugStates state)
-    {
-        bugStates = state;
-    }
+    
     void UpdateOrientation()
     {
         float t = pathTween.ElapsedPercentage();
@@ -78,9 +104,49 @@ public class BugController : Singleton<BugController>
         footPrintMat.SetFloat("_isFootPrint",1);
         Debug.Log("撞上");
     }
+    
 
+    //每次换界面也要初始化
+    public void InitializeWayPointsGroup()
+    {
+        GameObject wgp;
+        switch (uiType)
+        {
+            case  GameManager.UIType.NonDialogue:
+                wgp = Resources.Load("Prefab/WayPointsGroup/WayPointsGroup_NonDialogue") as GameObject;
+                if (wayPointsGroup!=null)
+                {
+                    Destroy(wayPointsGroup.gameObject);
+                    wayPointsGroup = null;
+                }
+                wayPointsGroup = Instantiate(wgp, GameObject.Find("UI").transform);
+                break;
+            case GameManager.UIType.Dialogue:
+                wgp = Resources.Load("Prefab/WayPointsGroup/WayPointsGroup_Dialogue") as GameObject;
+                if (wayPointsGroup!=null)
+                {
+                    Destroy(wayPointsGroup.gameObject);
+                    wayPointsGroup = null;
+                }
+                wayPointsGroup = Instantiate(wgp, GameObject.Find("UI").transform);
+                break;
+        }
+    }
+
+    #region UI
+    
     private void SHowPV()
     {
         UIManaer_HomePage_1.Instance.ShowBenginPV();
     }
+
+    private void SlideUI()
+    {
+        UIManager_Whole.Instance.MoveAwayWholeUI(UIManager_Whole.Instance.gameWindow_Content);
+        UIManager_Whole.Instance.MoveAwayWholeUI(this.gameObject);
+        UIManager_Whole.Instance.activeUIIndex += 1;
+    }
+
+
+    #endregion
 }
---
+++ BugEpic3/Assets/Scripts/UI/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    public PlayerState playerState = PlayerState.Active;
+    public enum PlayerState
+    {
+        Dialogue,Froze,Active
+    }
+    /// <summary>
+    /// 这一页UI荷马是否会说话
+    /// </summary>
+    public enum UIType
+    {
+        NonDialogue,Dialogue,Beginning
+    }
+    
+    
+}
